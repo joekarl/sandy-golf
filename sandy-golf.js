15a64e49a6a036422bead167bc40fbd07ae41537
Karl Kirch <kkirch@wdtinc.com>
2014-09-26 17:58:16 -0500

    Falling ball

--- sandy-golf.js
+++ sandy-golf.js
@@ -7,22 +7,42 @@ SG = (function(){
         //canvasHeight
         //debug
         //world
-        //physics
+        //physicsSpace
+        ballRadius: 5,
         PI_X2: 2 * Math.PI,
-        updateInterval : 15 //updates per second
+        time: 0,
+        updatesPerSecond : 15 //updates per second
     };
 
     function updateGame() {
+        //step physics (already fixed timestep so step by 1)
+        g.physicsSpace.step(1);
+
         var ball = g.world.ball;
         ball.update.call(ball);
     }
 
     function drawGame(dt, ctx) {
+        clearScreen(ctx);
+
+        //render ball
         var ball = g.world.ball;
         ctx.save();
-        //change coordinate system
-        ball.render.call(ball, dt, ctx);
+        var tx = ball.x + dt * ball.dx;
+        var ty = -ball.y + g.canvasHeight + dt * ball.dy;
+        ctx.translate(tx, ty);
+        ball.render.call(ball, ctx);
         ctx.restore();
+    }
+
+    function clearScreen(ctx) {
+        ctx.translate(0,0);
+        ctx.scale(1,1);
+        ctx.rotate(0);
+        ctx.fillStyle = "rgb(0,0,0)";
+        ctx.fillRect (0, 0, g.canvasWidth, g.canvasHeight);
+        ctx.fillStyle = undefined;
+        ctx.strokeStyle = undefined;
     }
 
     function initGame(canvas, debug) {
@@ -37,19 +57,36 @@ SG = (function(){
     }
 
     function initWorld() {
+        g.physicsSpace = new cp.Space();
+        g.physicsSpace.gravity = cp.v(0, -1);
+
+        var ballBody = g.physicsSpace.addBody(new cp.Body(100, 5));
+        ballBody.setPos(cp.v(50,250));
+        var ballShape = g.physicsSpace.addShape(new cp.CircleShape(ballBody, g.ballRadius, cp.v(50,250)));
+        ballShape.setFriction(0.7);
+
         g.world = {
             ball: {
-                x: 50,
-                y: 50,
-                radius: 5,
-                render: function(dt, ctx) {
+                //vx
+                //vy
+                //x
+                //y
+                dx: 0,
+                dy: 0,
+                body: ballBody,
+                render: function(ctx) {
                     ctx.beginPath();
-                    ctx.arc(this.x, this.y, this.radius, 0, g.PI_X2, false);
+                    ctx.arc(0, 0, g.ballRadius, 0, g.PI_X2, false);
                     ctx.fillStyle = 'white';
                     ctx.fill();
                 },
                 update: function() {
-
+                    var pos = this.body.getPos();
+                    var vel = this.body.getVel();
+                    this.x = pos.x;
+                    this.y = pos.y;
+                    this.dx = vel.x;
+                    this.dy = vel.y;
                 }
             }
         };
@@ -61,7 +98,7 @@ SG = (function(){
 
     function initGameLoop() {
         var loops = 0,
-            skipTicks = 1000 / g.updateInterval,
+            skipTicks = 1000 / g.updatesPerSecond, //in milliseconds
             maxFrameSkip = 10,
             nextGameTick = new Date().getTime()
             ctx = g.ctx2d,
